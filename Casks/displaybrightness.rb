82e1a6d7aae422c3e25e5280882f0b3dc2fb2567
Sedat Demirbaş <63248317+sdemirbas@users.noreply.github.com>
2026-04-01 01:30:39 +0300

    fix: correct SHA256 for v1.1.0 zip

--- Casks/displaybrightness.rb
+++ Casks/displaybrightness.rb
@@ -1,6 +1,6 @@
 cask "displaybrightness" do
   version "1.1.0"
-  sha256 "028123b640d0bad55fb94e90996952f1f98dbb6f1cb9003181f31c0e26dddefd"
+  sha256 "bc9a4ce404ef5dac6f1f71c00bdf824dff5edd1cc93c73ae22c04c9d26c3783d"
 
   url "https://github.com/sdemirbas/DisplaySettings/releases/download/v#{version}/DisplaySettings-#{version}.zip"
   name "DisplaySettings"
